d21e48c31fcb91df751ec65b884b60971ff6a631
B.Pardhu Chowdary <120440334+pardhu-B-chowdary@users.noreply.github.com>
2025-11-09 11:57:35 +0530

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = "myproject-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
